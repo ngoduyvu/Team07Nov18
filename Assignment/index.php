9aa60217dd2c9fe1aa333a5aabe844969907efdb
ngoduyvu <ngoduyvu@gmail.com>
2019-01-22 18:13:35 +1000

    Add files via upload

--- Assignment/index.php
+++ Assignment/index.php
@@ -37,28 +37,34 @@ include_once('include/config.php');
                 <li data-target="#demo" data-slide-to="1"></li>
                 <li data-target="#demo" data-slide-to="2"></li>
               </ul>
+             
               
-              <!-- The slideshow -->
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <a href="movie_detail.php?id=1"><img src="img/movie/Alita_poster.jpg" width="900" height="500"></a>
-                </div>
-                <div class="carousel-item">
-                  <a href="movie_detail.php?id=3"><img src="img/movie/Aquaman_poster.jpg" width="900" height="500"></a>
-                </div>
-                <div class="carousel-item">
-                  <a href="movie_detail.php?id=5"><img src="img/movie/Bohemian-Rhapsody_poster.jpg" width="900" height="500"></a>
-                </div>
-              </div>
-              
-              <!-- Left and right controls -->
-              <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-              </a>
-              <a class="carousel-control-next" href="#demo" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-              </a>
-              
+              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="img/movie/Alita_poster.jpg" alt="First slide" width="900" height="500">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="img/movie/Aquaman_poster.jpg" alt="Second slide" width="900" height="500">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="img/movie/Bohemian-Rhapsody_poster.jpg" alt="Third slide" width="900" height="500">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+			</div>
               
               <!-- Display Movies -->
               <dev style="display: inline-block;">
@@ -72,12 +78,22 @@ include_once('include/config.php');
                   <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Mortal-Engines-2018.jpeg"></a>
                   <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/How-to-Train-Your-Dragon-The-Hidden-World-2019.jpeg"></a>
              </dev>
-              
-              <h2 style="margin-botton: 0px; padding-bottom: 0px;">Most Watching</h2>
+       
+              <h2 style="margin-botton: 0px; padding-bottom: 0px;">Current Popular</h2>
               <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Spider-Man-Into-the-Spider-Verse-2018.jpeg"></a>
               <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Aquaman-2018.jpeg"></a>
               <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/the-grinch.jpeg"></a>
               <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Serenity-2019.jpg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/If-Beale-Street-Could-Talk-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Widows-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Green-Book-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Mary-Queen-of-Scots-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/On-the-Basis-of-Sex-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Robin-Hood-2018.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/The-Kid-Who-Would-Be-King-2019.jpeg"></a>
+              <a href="movie_detail.php?id=1"><img class="movies_poster" src="img/movie/Instant-Family-2018.jpeg"></a>
+              
+    
                 
                 
                 
