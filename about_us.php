ec5d0fe2ecdf9941aca9ff3b2116551ad69e37b4
JC437895 (Frank YIN) <45928772+jc437895@users.noreply.github.com>
2019-01-23 20:44:07 +1000

    about_us from frank

---
+++ about_us.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sign Up</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+	<!-- Loading main css file -->
+	<link rel="stylesheet" href="style/style.css">
+
+<title>About Us | The Team7 Media</title>
+
+<body>
+    <div class ="site-container">
+    	<!------------------- Header Part ------------------->
+    	<?php include("include/header.inc") ?>
+        
+        <!------------------- Contain Part ------------------->
+        <main class="main-content">
+        	<div class="container">
+<div class="custom-page-content"><div class="section-boxed" id="section-1"><h1 class="page-heading">About Us</h1>
+<div class="custom-page-content"><div class="section-boxed" id="section-1"><h2 class="page-heading">The Team7 Media</h2>
+<div class="services-wrapper services-boxed-photo cols-wrapper cols-1" id="section-2"><div class="services-box col"><div class="img-container"><img src="img/about_us.jpg" >
+<p>&nbsp;</p>
+<p>We love movies! Team7 Media was created for people who want to watch "Great Movies. Affortable." Team7 Media has full-length movies that will make you laugh, make you cry, scare the heck out of you, or inspire you to hug the person you love. We have compelling documentaries, foreign films, unique original web series, and a place that showcases the brightest film makers of tomorrow. Team7 meida is the most user friendly movie destination on the web. No movie is more than two clicks away and they are all Super value. </p>
+<p>&nbsp;</p>
+
+<h2 class="page-heading">The Team</h2>
+<p>The team of Team7 Media, we are attending the same campus, James Cook University Brisbane, and have the same degree, Master of Information Technology. That being said, each of us &#8211;for some unknown, weird, complicated and unique reasons&#8211; is now a Web developer 🙂</p>
+<p>&nbsp;</p>
+<div class="services-wrapper services-boxed-photo cols-wrapper cols-4" id="section-2"><div class="services-box col"><div class="img-container"><img src="img/duyvu.jpg" alt="Yuniar Setiawan"/></div><div class="services-content"><div class="sc-wraper"><h3>Duy Vu Ngo</h3><p>Template, Homepage, Search</p></div></div></div><p>&nbsp;</p>
+
+<div class="services-box col"><div class="img-container"><img src="img/chimi.jpg" alt="Andar Redityo"/></div><div class="services-content"><div class="sc-wraper"><h3>Chimi DORJI</h3><p>Subscription</p></div></div></div><p>&nbsp;</p>
+
+<div class="services-box col"><div class="img-container"><img src="img/kinley.jpg"  alt="Sabrian Rizqi"/></div><div class="services-content"><div class="sc-wraper"><h3>Kinley NAMGAY</h3><p>Movie Categories</p></div></div></div> <p>&nbsp;</p>
+
+<div class="services-box col nomargin"><div class="img-container"><img src="img/qichang.jpg" alt="Imam Rozi"/></div><div class="services-content"><div class="sc-wraper"><h3>Qichang YIN</h3><p>Sign up, Login, Contact us, About us</p></div></div></div>
+<div class="clear"></div></div>
+<p>&nbsp;</p>
+<h2 class="page-heading">Our contact details</h2>
+<p>349 Queen Street, Brisbane City, QLD4000<br />
+Emaail: Team7_media@gmail.com<br />
+</p>
+</div></div></div> <!-- end main content holder (#content/#full-width) -->
+<div class="clear"></div>
+</div> <!-- end #content-container -->
+</div>
+</div>
+</main>
+<!------------------- Footer Part ------------------->
+	<?php include("include/footer.inc") ?>
+</body>
+</html>
